1d85f16507c00a59100f4d7a52cb63866eeb6846
siwen.liu319@outlook.com <siwen.liu319@outlook.com>
2023-04-04 22:13:34 +0200

    svr

--- assignment1/svr.py
+++ assignment1/svr.py
@@ -1,3 +1,6 @@
-import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd
+import numpy as np
+
+x = np.linspace(0, 20, 100)  # Create a list of evenly-spaced numbers over the range
+plt.plot(x, np.sin(x))       # Plot the sine of each x point
+plt.show()  
